8b09b795ebe3151a40eabf9cb854fd5d81e6ecb7
RomLabo <82595594+RomLabo@users.noreply.github.com>
2024-11-11 00:34:44 +0100

    mise en place chat simple et interface basique

--- client.c
+++ client.c
@@ -15,101 +15,123 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <gtk/gtk.h>
+
+/*
+    Constantes préprocesseur
+*/
+
+// Paramètres socket
+#define SOCK_DOMAIN AF_INET
+#define SOCK_TYPE SOCK_STREAM
+#define SOCK_PROTOCOL 0
+
+#define PORT 8080
+#define BUFFER_SIZE 512
+
+/*
+    Variables
+*/
+
+char buffer[BUFFER_SIZE];
+int socket_client;
+GtkBuilder *builder;
+GObject *window;
+GObject *button;
+GtkWidget *entry;
 
 /* 
     Déclaration des fonctions
 */
 
-void create_socket(int fd);
-struct sockaddr_in config_socket(char *addr, char *port);
-void connect_to_server(int fd, struct sockaddr *ptr_addr, int lg_addr);
+static void quit_app(GtkWidget *widget, gpointer data);
+static void send_msg(GtkWidget *widget, gpointer data);
 
 /*
     Main
 */
 
 int main(int argc, char *argv[]) {
-    int fd;
-    char msg;
-    char acquit[3];
-    char chaine[4];
-    char acquitMsg[3] = "OK";
-    char reponse[4];
+    GError *error = NULL;
 
-    printf("Client TCP \n");
-
-    if (argc == 4) {
-
-        /*if ((fd = socket(AF_INET,SOCK_STREAM,0)) == -1) {
-            perror("socket");
-            exit(1);
-        }*/
-
-        struct sockaddr_in sock;
-        sock = config_socket(argv[1], argv[2]);
-        /*sock.sin_addr.s_addr = inet_addr(argv[1]);
-        sock.sin_family = AF_INET;
-        sock.sin_port = htons(atoi(argv[2]));*/
-
-        /* Etablir la connection */
-        connect_to_server(fd, (struct sockaddr*)&sock, sizeof(sock));
-        /*if (connect(fd, (struct sockaddr*)&sock,sizeof(sock)) == -1) {
-            perror("connect");
-            exit(1);
-        }*/
-
-        int cle = atoi(argv[3]);
-
-        /* Envoyer la cle (un entier ici )*/
-        if (write(fd,&cle,sizeof(cle)) == -1) {
-            perror("Envoie de la clé impossible");
-            exit(1);
-        }
-
-
-        /* Attente de l'acquittement */
-        while (strcmp(acquit,acquitMsg) != 0) {
-            if (read(fd,&acquit,3) == -1) {
-                perror("Lecture acquittement impossible");
-                exit(1);
-            }
-        }
-
-        printf("Saisir le mode : ");
-        scanf("%c", &msg);
-
-        /* Envoie du mode */
-        if (write(fd,&msg,sizeof(msg)) == -1) {
-            perror("write");
-            exit(1);
-        }
-        printf("Mode envoyé : %c\n", msg);
-
-        printf("Saisir la chaine : ");
-        scanf("%3[^\n]", chaine);
-
-        /* Envoie du mode */
-        if (write(fd,&chaine,sizeof(chaine)) == -1) {
-            perror("write");
-            exit(1);
-        }
-        printf("Chaine envoyé : %s\n", chaine);
-
-        /* Recevoir la reponse */
-        if (read(fd,&reponse,sizeof(reponse)) == -1) {
-            perror("recvfrom");
-            exit(1);
-        }
-
-        printf("Reponse : %s\n", reponse);
-
-
-        /* Fermer la connexion */
-        shutdown(fd,2);
-        close(fd);
-    } else {
+    /* Vérification des arguments */
+    if (argc != 3) { 
         printf("Veuillez renseigner ip et port \n");
+        return EXIT_FAILURE;
     }
+
+    /* Création socket client */
+    if ((socket_client = socket(SOCK_DOMAIN, SOCK_TYPE, SOCK_PROTOCOL)) == -1) {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    /* Configuration socket serveur */
+    struct sockaddr_in socket_server;
+    socket_server.sin_addr.s_addr = inet_addr(argv[1]);
+    socket_server.sin_family = SOCK_DOMAIN;
+    socket_server.sin_port = htons(atoi(argv[2]));
+
+    /* Création de la connection */
+    if (connect(socket_client, (struct sockaddr*)&socket_server,sizeof(socket_server)) == -1) {
+        perror("connect");
+        return EXIT_FAILURE;
+    }
+
+    /* Attente de l'acquittement */
+    while (strcmp(buffer, "ACK") != 0) {
+        int nb_char = recv(socket_client, buffer, BUFFER_SIZE -1, 0);
+        if (nb_char <= 0) {
+            perror("Lecture acquittement impossible");
+            break;
+        }
+        buffer[nb_char] = '\0';
+    }
+
+    gtk_init (&argc, &argv);
+
+    /* Récupération des elements builder.ui */
+    builder = gtk_builder_new ();
+    if (gtk_builder_add_from_file (builder, "interface.ui", &error) == 0) {
+        g_printerr ("Error loading file: %s\n", error->message);
+        g_clear_error (&error);
+        return 1;
+    }
+
+    /* Création des différents signaux */
+    window = gtk_builder_get_object (builder, "window");
+    g_signal_connect (window, "destroy", G_CALLBACK (quit_app), NULL);
+
+    entry = GTK_WIDGET(gtk_builder_get_object(builder, "searchentry"));
+
+    button = gtk_builder_get_object (builder, "send");
+    g_signal_connect (button, "clicked", G_CALLBACK (send_msg), entry);
+
+    button = gtk_builder_get_object (builder, "quit");
+    g_signal_connect (button, "clicked", G_CALLBACK (quit_app), NULL);
+
+    /* Démarrage de la boucle principale Gtk */
+    gtk_main ();
+
+    /*while (1) {
+        printf("Saisir un message ('exit' pour quitter) : ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if (send(socket_client, buffer, strlen(buffer), 0) < 0) {
+            perror("Erreur envoi message\n");
+            break;
+        }
+
+        if (strcmp(buffer, "exit") == 0) {
+            printf("Déconnexion du serveur\n");
+            break;
+        }
+    }*/
+
+    /* Fermeture de la connexion */
+    close(socket_client);
     return 0;
 }
 
@@ -117,24 +139,23 @@ int main(int argc, char *argv[]) {
     Définition des fonctions
 */
 
-void create_socket(int fd) {
-    if ((fd = socket(AF_INET,SOCK_STREAM,0)) == -1) {
-        perror("Creation socket impossible");
-        exit(1);
+static void quit_app(GtkWidget *widget, gpointer data) {
+    strncpy(buffer, "exit", BUFFER_SIZE -1);
+    buffer[BUFFER_SIZE -1] = '\0';
+    if (send(socket_client, buffer, strlen(buffer), 0) < 0) {
+        perror("Erreur envoi message\n");
     }
+    close(socket_client);
+    gtk_main_quit();
 }
 
-struct sockaddr_in config_socket(char* addr, char* port) {
-    struct sockaddr_in sock;
-    sock.sin_addr.s_addr = inet_addr(addr);
-    sock.sin_family = AF_INET;
-    sock.sin_port = htons(atoi(port));
-    return sock;
-}
-
-void connect_to_server(int fd, struct sockaddr *ptr_addr, int lg_addr) {
-    if (connect(fd, ptr_addr, lg_addr) == -1) {
-        perror("Connexion impossible");
-        exit(1);
+static void send_msg(GtkWidget *widget, gpointer data) {
+    GtkWidget *entry = GTK_WIDGET(data);
+    const char *text = gtk_entry_get_text(GTK_ENTRY(entry));
+    
+    if (send(socket_client, text, strlen(text), 0) < 0) {
+        perror("Envoi message impossible");
     }
+
+    gtk_entry_set_text(GTK_ENTRY(entry), "");
 }
